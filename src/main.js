0fa97cd8f9ffa2bbbd5de135ebbfb01ef9378575
Gandoki <abahgideon111@gmail.com>
2024-04-11 23:35:44 -0700

    attended to some vital issues

--- src/main.js
+++ src/main.js
@@ -10,13 +10,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAuEuC9oYVjTAL-VPe56Ar4Dsc3SRSfspY",
-    authDomain: "msapp-5a44e.firebaseapp.com",
-    projectId: "msapp-5a44e",
-    storageBucket: "msapp-5a44e.appspot.com",
-    messagingSenderId: "335257294942",
-    appId: "1:335257294942:web:7285b1915b555dfba5fccf",
-    measurementId: "G-MMBVGZRHJ4"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
   
   initializeApp(firebaseConfig);
