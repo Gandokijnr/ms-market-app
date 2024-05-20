aecd0e15b427e306125208c1b2f0147d0ca30c9d
Gandoki <abahgideon111@gmail.com>
2024-05-20 03:59:34 -0700

    stop tracking config file

--- src/main.js
+++ src/main.js
@@ -10,13 +10,11 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAuEuC9oYVjTAL-VPe56Ar4Dsc3SRSfspY",
-  authDomain: "msapp-5a44e.firebaseapp.com",
-  projectId: "msapp-5a44e",
-  storageBucket: "msapp-5a44e.appspot.com",
-  messagingSenderId: "335257294942",
-  appId: "1:335257294942:web:7285b1915b555dfba5fccf",
-  measurementId: "G-MMBVGZRHJ4"
+
+  
+  // include your own config file from firebase to use this app
+
+
   };
   
   initializeApp(firebaseConfig);
